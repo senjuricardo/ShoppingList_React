a10d516ada547c045383ecf90d54157b391eae17
Ricardo <ricardo.sousa13@hotmail.com>
2023-02-07 23:15:45 +0000

    added fetching data to an api

--- src/components/CheckList/Checklist.js
+++ src/components/CheckList/Checklist.js
@@ -1,41 +1,48 @@
 import React from "react"
-import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from "react"
 import './CheckList.css'
 const CheckList = (props) => {
     const [sum, setSum] = useState(0);
-    
-     const deleteItem = () => {
-       alert('COMING IN NEXT RELEASE, BE PATIENT YOU IDIOT')
-     };
+
+    const deleteItem = (index) => {
+        const updatedList = [...props.itemsList];
+        const removedItem = updatedList.splice(index, 1);
+        setSum(sum - removedItem[0].price * removedItem[0].amount);
+        props.setListItems(updatedList);
+    };
+    // const deleteItem = (itemToDelete) => {
+    //     const updatedList = props.itemsList.filter(item => item.item !== itemToDelete.item);
+    //     props.setListItems(updatedList);
+    // };
+
+
     useEffect(() => {
-        const total = props.itemsList.reduce((acc = 0, item) => (acc + parseFloatgitt(item.price) * item.amount) , 0);
+        const total = props.itemsList.reduce((acc = 0, item) => (acc + parseFloat(item.price) * item.amount), 0);
         setSum(total);
-      }, [props.itemsList]);
+    }, [props.itemsList]);
 
 
     return (
-        <div>
-
-        
-        <div className="Checklist">
-             {Object.keys(props.itemsList).length !== 0 && <div className="headerList"><h3>ITEM</h3><h3>AMOUNT</h3><h3>PRICE/unit</h3></div>}
-            {/* <button onClick={handleClick}>click me</button> */}
-            <ul >
-                {props.itemsList.map(item => (
-                    <li className="bodyList"  key={uuidv4()}>
-                        <div className="bodyList">
-                        <div className="item">{item.item}</div>
-                        <div className="amount">{item.amount}</div>
-                        <div className="price">{item.price}</div> 
-                        </div>
-                        <button className="button3" onClick={deleteItem}>DELETE</button>
+        <>
+            <div className="Checklist">
+                {Object.keys(props.itemsList).length !== 0 && <div className="headerList"><h3>ITEM</h3><h3>AMOUNT</h3><h3>PRICE/unit</h3></div>}
+                {/* <button onClick={handleClick}>click me</button> */}
+                <ul >
+                    {props.itemsList.map((item, index) => (
+                        <li className="bodyList" key={index}>
+                            <div className="bodyList">
+                                <div className="item">{item.item}</div>
+                                <div className="amount">{item.amount}</div>
+                                <div className="price">{item.price}</div>
+                            </div>
+                            <button className="button3" onClick={() => deleteItem(index)}>DELETE</button>
                         </li>
-                ))}
-            </ul>
-        </div>
+                    ))}
+
+                </ul>
+            </div>
             {Object.keys(props.itemsList).length !== 0 && <div className="totalDiv"><p className="total">TOTAL TO PAY: {sum}€</p></div>}
-        </div>
+        </>
     )
 
 }
--- src/containers/InputBuilder.js
+++ src/containers/InputBuilder.js
@@ -1,10 +1,14 @@
 import React from "react"
 import './InputBuilder.css'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import CheckList from "../components/CheckList/Checklist"
 const InputBuilder = () => {
     const [formData, setFormData] = useState({ item: '', amount: '', price: '' });
     const [ListItems, setListItems] = useState([])
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+
 
     const handleChange = (event) => {
         setFormData({ ...formData, [event.target.name]: event.target.value });
@@ -12,13 +16,49 @@ const InputBuilder = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setListItems([...ListItems, formData]);
-        console.log(ListItems)
-        console.log(formData);
+        if (!formData.price || formData.price < 0) {
+            alert('please insert a price')
+        }
+        else {
+            const sameItem = ListItems.findIndex(item => item.item === formData.item);
+            if (sameItem >= 0) {
+                const updatedListItems = [...ListItems];
+                updatedListItems[sameItem].amount = Number(updatedListItems[sameItem].amount) + Number(formData.amount);
+                // updatedListItems[sameItem].price = Number(formData.price);
+                setListItems(updatedListItems);
+
+            } else {
+                setListItems([...ListItems, formData]);
+            }
+            setFormData({ item: '', amount: '', price: '' });
+        }
+
     };
 
+    useEffect(() => {
+        setLoading(true);
+        fetch("https://dummyjson.com/products")
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+                setData(result);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+      if (error) {
+        return <div>An error occurred: {error.message}</div>;
+      }
     return (
         <div >
+            
             <form className="InputBuilder" onSubmit={handleSubmit}>
                 <div className="input">
                     <label>Item</label>
@@ -33,8 +73,8 @@ const InputBuilder = () => {
                     <input type="number" name="price" value={formData.price} onChange={handleChange} placeholder="Insert a prince of an item" />
                 </div>
                 <button type="submit" className="glow-on-hover">Submit</button>
-            </form> 
-            <CheckList itemsList={ListItems}/>  
+            </form>
+            <CheckList itemsList={ListItems} setListItems={setListItems} />
         </div>
     );
 
